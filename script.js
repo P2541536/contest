0eefa032629d9668f34f6f4d52164234df0ebb52
P2541536 <119421978+P2541536@users.noreply.github.com>
2022-12-30 23:57:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function CONTACTUS(){
+    let Name=document.getElementById('Name').checked;
+    let LastName=document.getElementById('Last Name').checked;
+    let EmailAddress=document.getElementById('Email address').checked;
+    let ReasonForContracting=document.getElementById('Reason For Contracting').checked;
+    if(Name!=null && LastName!=null && EmailAddress!=null && ReasonForContracting!=null)
+
+    alert("for Submitted Sucsessfuiiy!")
+}
